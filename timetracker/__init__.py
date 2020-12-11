71ebae4ad0e9bc840378dcff95fe161e47c49b18
Fyssion <fyssioncodes@gmail.com>
2020-12-11 09:58:34 -0600

    Bump version to 0.1.0

--- timetracker/__init__.py
+++ timetracker/__init__.py
@@ -6,7 +6,7 @@ from . import utils
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
 
-__version__ = "0.1.0a"
+__version__ = "0.1.0"
 __author__ = "Fyssion"
 __copyright__ = "Copyright 2020 Fyssion"
 __license__ = "MIT"
