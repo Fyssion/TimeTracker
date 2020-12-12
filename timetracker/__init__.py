442372a66f16af65c62140fed071fb67ef6692fe
Fyssion <fyssioncodes@gmail.com>
2020-12-11 20:51:42 -0600

    Bump version to 0.2.3

--- timetracker/__init__.py
+++ timetracker/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
+version_info = VersionInfo(major=0, minor=2, micro=3, releaselevel="final", serial=0)
 
 __version__ = "0.2.2"
 __author__ = "Fyssion"
